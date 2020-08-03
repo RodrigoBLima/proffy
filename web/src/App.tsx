626d23125b3c362d895a83701bf4b62a94535c7a
Rodrigo Barbosa <rodrigo.lima@agriconnected.com>
2020-08-03 11:42:04 -0300

    landing page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Landing from './pages/Landing';
+import './assets/styles/global.css'
 
 function App() {
   return (
     <div>
-     app
+     <Landing />
     </div>
   );
 }
---
+++ web/src/pages/Landing/index.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import LogoImg from "../../assets/images/logo.svg";
+import landingImg from "../../assets/images/landing.svg";
+
+import studyIcon from "../../assets/images/icons/study.svg";
+import giveClassesIcon from "../../assets/images/icons/give-classes.svg";
+
+import purpleHeartIcon from "../../assets/images/icons/purple-heart.svg";
+
+import "./styles.css";
+
+export default function Landing() {
+  return (
+    <div id="page-landing">
+      <div id="page-landing-content" className="container">
+        <div className="logo-container">
+          <img src={LogoImg} alt="" />
+          <h2>Sua plafaforma de estudos online.</h2>
+        </div>
+        <img src={landingImg} alt="" className="hero-image" />
+        <div className="buttons-container">
+          <a href="" className="study">
+            <img src={studyIcon} alt="Estudar" />
+            Estudar
+          </a>
+          <a href="" className="give-classes">
+            <img src={giveClassesIcon} alt="Dar aulas" />
+            Dar aulas
+          </a>
+        </div>
+        <span className="total-connections">
+          Total de 22 conexões já realizadas
+          <img src={purpleHeartIcon} alt="Coração roxo" />
+        </span>
+      </div>
+    </div>
+  );
+}
